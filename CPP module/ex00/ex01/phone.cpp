6ba587efb79c83652c4665bde3622291f9ef5467
lamlaika rayane <139387044+raynlamlaika@users.noreply.github.com>
2024-10-14 16:21:45 +0100

    Add files via upload
    first

---
+++ CPP module/ex00/ex01/phone.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+
+class phone
+{
+	public:
+		void add_contact(char* firstname ,char* lastname,cha* nickname,int number,char* secret);
+		void exit();
+		char** search(char*name, char** list);
+		phone(string name)
+		{
+			name = name;
+		}
+
+};
+
+void add_contact(char* firstname ,char* lastname,cha* nickname,int number,char* secret)
+{
+	return;
+}
+
+void phone::exit()
+{
+	return;
+}
+
+int main(int ac, char* av[])
+{
+	phone tele;
+	tele.cntact = "HADA RAYN";
+	std ::cout << tele.cntact << "\n";
+
+}
+
+
+
+
+
+
+
